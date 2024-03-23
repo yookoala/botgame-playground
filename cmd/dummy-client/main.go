d063a56823fcb7e60a11db9d4a7ebc441a1e450d
Koala Yeung <koalay@gmail.com>
2024-03-24 00:05:22 +0800

    dummy-client: can run indefinately
    * Accept interrupt.
    * Repeatedly send server the same message until keyboard interrupt.

--- cmd/dummy-client/main.go
+++ cmd/dummy-client/main.go
@@ -2,9 +2,27 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"time"
 )
+
+func writeMsg(conn net.Conn, msg []byte) (err error) {
+	// Send a message to the socket
+	_, err = conn.Write(msg)
+	return
+}
+
+func waitErrorOnce(fn func() error) <-chan error {
+	ch := make(chan error)
+	go func() {
+		ch <- fn()
+	}()
+	return ch
+}
 
 func main() {
 	// Connect to ./echo.sock
@@ -30,7 +48,38 @@ func main() {
 	// Print the received message
 	log.Println("Received:", message)
 
-	// Send a message to the socket
-	conn.Write([]byte("Hello, server!\n"))
-	conn.Close()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			// Check if sig is interrupt (Ctrl+C)
+			if sig.String() == "interrupt" {
+				conn.Close()
+			} else {
+				fmt.Printf("Received signal: %s\n", sig.String())
+			}
+		}
+	}()
+
+	for {
+		select {
+		case <-time.After(10 * time.Second):
+		case err := <-waitErrorOnce(func() error {
+			return writeMsg(conn, []byte("Hello, server!\n"))
+		}):
+			if err == nil {
+				time.Sleep(1 * time.Second)
+				continue
+			}
+
+			switch err.(type) {
+			case *net.OpError:
+				log.Print("Socket closed. Quit")
+				os.Exit(0)
+			default:
+				log.Printf("Socket error: %v", err)
+				os.Exit(1)
+			}
+		}
+	}
 }
